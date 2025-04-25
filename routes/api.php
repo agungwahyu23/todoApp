03e9805765d9134d977d564871ef94c7d08c27aa
agungwahyu23 <52806335+agungwahyu23@users.noreply.github.com>
2025-04-26 00:22:56 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,5 +23,10 @@ Route::prefix('v1')->group(function () {
 
     Route::middleware('auth:api')->group( function () {
         Route::post('/logout', [AuthController::class, 'logout']);
+        Route::get('/tasks', [TaskController::class, 'index']);
+        Route::post('/tasks', [TaskController::class, 'store']);
+        Route::get('/tasks/{id}', [TaskController::class, 'show']);
+        Route::put('/tasks/{id}', [TaskController::class, 'update']);
+        Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
     });
 });
